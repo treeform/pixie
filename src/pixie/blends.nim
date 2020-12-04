bed384db6a56bcf28773098fffe91282d13bf632
Ryan Oldenburg <guzba8@gmail.com>
2020-12-03 20:21:15 -0600

    add links to blend formula sources

--- src/pixie/blends.nim
+++ src/pixie/blends.nim
@@ -1,6 +1,9 @@
 ## Blending modes.
 import chroma, math, algorithm
 
+# See https://www.w3.org/TR/compositing-1/
+# See https://www.khronos.org/registry/OpenGL/extensions/KHR/KHR_blend_equation_advanced.txt
+
 type BlendMode* = enum
   bmNormal
   bmDarken
