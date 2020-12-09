bb80426a4d2552d690272cb5357daf87a453b267
Ryan Oldenburg <guzba8@gmail.com>
2020-12-08 18:12:27 -0600

    f

--- tests/test_png.nim
+++ tests/test_png.nim
@@ -17,10 +17,10 @@ for channels in 1 .. 4:
   var data: seq[uint8]
   for x in 0 ..< 16:
     for y in 0 ..< 16:
-        var components = newSeq[uint8](channels)
-        for i in 0 ..< channels:
-          components[i] = (x * 16).uint8
-        data.add(components)
+      var components = newSeq[uint8](channels)
+      for i in 0 ..< channels:
+        components[i] = (x * 16).uint8
+      data.add(components)
   let encoded = encodePng(16, 16, channels, data[0].addr, data.len)
 
 for file in pngSuiteCorruptedFiles:
