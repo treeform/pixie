10a10d3f272f5fda2f5ac257a2d82c50e5b22cc6
Ryan Oldenburg <guzba8@gmail.com>
2020-12-19 21:59:43 -0600

    svg more basic shapes, more transforms

--- tests/test_svg.nim
+++ tests/test_svg.nim
@@ -1,6 +1,12 @@
 import pixie/fileformats/svg, pixie, strformat
 
 const files = [
+  "line01",
+  "polyline01",
+  "polygon01",
+  "rect01",
+  "rect02",
+  "circle01",
   "triangle01",
   "quad01",
   "Ghostscript_Tiger"
