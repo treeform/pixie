29ca5501bf185476b5bcf4edca4fb7646a76e4f3
treeform <starplant@gmail.com>
2021-02-13 20:13:58 -0800

    drawCorrect vs drawUber benchmark.

---
+++ tests/benchmark_drawCorrect_vs_drawUber.nim
@@ -0,0 +1,47 @@
+import chroma, vmath, benchy
+include pixie/images
+
+block:
+  let
+    a = newImage(1000, 1000)
+    b = newImage(50, 50)
+  a.fill(rgba(255, 0, 0, 255))
+  b.fill(rgba(0, 255, 0, 255))
+
+  timeIt "drawCorrect small-on-big":
+    a.drawCorrect(b, translate(vec2(25, 25)), bmNormal)
+    keep(b)
+
+block:
+  let
+    a = newImage(1000, 1000)
+    b = newImage(50, 50)
+  a.fill(rgba(255, 0, 0, 255))
+  b.fill(rgba(0, 255, 0, 255))
+
+  timeIt "drawUber small-on-big":
+    a.drawUber(b, translate(vec2(25, 25)), bmNormal)
+    keep(b)
+
+
+block:
+  let
+    a = newImage(1000, 1000)
+    b = newImage(50, 50)
+  a.fill(rgba(255, 0, 0, 255))
+  b.fill(rgba(0, 255, 0, 255))
+
+  timeIt "drawCorrect small-on-big smooth":
+    a.drawCorrect(b, translate(vec2(25.1, 25.1)), bmNormal)
+    keep(b)
+
+block:
+  let
+    a = newImage(1000, 1000)
+    b = newImage(50, 50)
+  a.fill(rgba(255, 0, 0, 255))
+  b.fill(rgba(0, 255, 0, 255))
+
+  timeIt "drawUber small-on-big smooth":
+    a.drawUber(b, translate(vec2(25.1, 25.1)), bmNormal)
+    keep(b)
