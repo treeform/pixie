ec31b0c383b04dcf0fef2a972da25b258f42f102
Ryan Oldenburg <guzba8@gmail.com>
2021-05-09 17:27:34 -0500

    trailing data test for png

--- tests/test_png.nim
+++ tests/test_png.nim
@@ -1,4 +1,4 @@
-import pixie/common, pixie/fileformats/png, pngsuite, strformat
+import pixie, pixie/fileformats/png, pngsuite, strformat
 
 # for file in pngSuiteFiles:
 #   let
@@ -13,19 +13,23 @@ import pixie/common, pixie/fileformats/png, pngsuite, strformat
 #   doAssert decoded.width == decoded2.width
 #   doAssert decoded.data == decoded2.data
 
-for channels in 1 .. 4:
-  var data: seq[uint8]
-  for x in 0 ..< 16:
-    for y in 0 ..< 16:
-      var components = newSeq[uint8](channels)
-      for i in 0 ..< channels:
-        components[i] = (x * 16).uint8
-      data.add(components)
-  let encoded = encodePng(16, 16, channels, data[0].addr, data.len)
+block:
+  for channels in 1 .. 4:
+    var data: seq[uint8]
+    for x in 0 ..< 16:
+      for y in 0 ..< 16:
+        var components = newSeq[uint8](channels)
+        for i in 0 ..< channels:
+          components[i] = (x * 16).uint8
+        data.add(components)
+    let encoded = encodePng(16, 16, channels, data[0].addr, data.len)
 
-for file in pngSuiteCorruptedFiles:
-  try:
-    discard decodePng(readFile(&"tests/images/png/pngsuite/{file}.png"))
-    doAssert false
-  except PixieError:
-    discard
+  for file in pngSuiteCorruptedFiles:
+    try:
+      discard decodePng(readFile(&"tests/images/png/pngsuite/{file}.png"))
+      doAssert false
+    except PixieError:
+      discard
+
+block:
+  discard readImage("tests/images/png/trailing_data.png")
