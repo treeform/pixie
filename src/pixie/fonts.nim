236a9c0ea77ebdda6a234baa61c7fff4426f9f4a
Ryan Oldenburg <guzba8@gmail.com>
2021-05-11 15:05:34 -0500

    span fixes, more tests

--- src/pixie/fonts.nim
+++ src/pixie/fonts.nim
@@ -199,11 +199,9 @@ proc typeset*(
           at.y += 1
           prevCanWrap = 0
           lines[^1][1] = runeIndex
-          lines.add((runeIndex + 1, 0))
+          if runeIndex + 1 < result.runes.len:
+            lines.add((runeIndex + 1, 0))
         else:
-          if rune.canWrap():
-            prevCanWrap = runeIndex
-
           let advance = advance(font, result.runes, runeIndex)
           if wrap and rune != SP and bounds.x > 0 and at.x + advance > bounds.x:
             # Wrap to new line
@@ -222,6 +220,9 @@ proc typeset*(
 
             lines[^1][1] = lineStart - 1
             lines.add((lineStart, 0))
+
+          if rune.canWrap():
+            prevCanWrap = runeIndex
 
           result.positions[runeIndex] = at
           result.selectionRects[runeIndex] = rect(at.x, at.y, advance, 0)
@@ -302,13 +303,12 @@ proc typeset*(
               font.defaultLineHeight
         lineHeights[line] = max(lineHeights[line], fontLineHeight)
         for runeIndex in start .. stop:
-          if line + 1 < lines.len and runeIndex == lines[line][1]:
+          if line + 1 < lines.len and runeIndex == lines[line + 1][0]:
             inc line
             lineHeights[line] = max(lineHeights[line], fontLineHeight)
         # Handle when span and line endings coincide
         if line + 1 < lines.len and stop == lines[line][1]:
           inc line
-          lineHeights[line] = max(lineHeights[line], fontLineHeight)
 
     block: # Vertically position the glyphs
       var
