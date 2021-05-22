d5604ea5126d031fbb19171b86c26859934cef32
treeform <starplant@gmail.com>
2021-05-22 15:29:25 -0700

    f

---
+++ tests/test_arch.nim
@@ -0,0 +1,5 @@
+echo "size of int: ", sizeof(int), " bytes"
+echo "size of cint: ", sizeof(cint), " bytes"
+var v: int
+echo "sise of pointer: ", sizeof(v.addr), " bytes"
+echo "endianness of the CPU: ", cpuEndian
