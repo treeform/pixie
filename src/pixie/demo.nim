49a281d2100c7f09265a1c694a2f4c8af61e5e0c
treeform <starplant@gmail.com>
2021-05-26 17:41:52 -0700

    Fix demo imports.

--- src/pixie/demo.nim
+++ src/pixie/demo.nim
@@ -1,6 +1,8 @@
-import opengl, pixie, pixie/context, staticglfw except Image
+import opengl, pixie, pixie/context
+import staticglfw except Image
 
-export pixie, staticglfw except Image
+export pixie
+export staticglfw except Image
 
 var
   dpi: float32 = 1.0
