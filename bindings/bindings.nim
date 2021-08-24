380a8418ec2487207c6f8cab621a1cedc32ef8a8
Ryan Oldenburg <ryan@guzba.com>
2021-08-23 20:34:18 -0500

    f

--- bindings/bindings.nim
+++ bindings/bindings.nim
@@ -42,168 +42,181 @@ exportEnums [
 exportObjects [Vector2, Matrix3, Rect, Color, ColorStop, TextMetrics]
 
 exportProcs:
-  export bindings.checkError
-  export bindings.takeError
+  export
+    bindings.checkError,
+    bindings.takeError
 
 exportSeq seq[float32]:
   discard
 
 exportSeq seq[Span]:
-  export pixie.typeset
-  export pixie.computeBounds
+  export
+    pixie.typeset,
+    pixie.computeBounds
 
 exportRefObject Image, ["width", "height"]:
-  export pixie.writeFile
-  export pixie.wh
-  export pixie.copy
-  export pixie.getColor
-  export pixie.setColor
-  export pixie.fill
-  export pixie.flipHorizontal
-  export pixie.flipVertical
-  export pixie.subImage
-  export pixie.minifyBy2
-  export pixie.magnifyBy2
-  export pixie.applyOpacity
-  export pixie.invert
-  export pixie.blur
-  export pixie.newMask
-  export pixie.resize
-  export pixie.shadow
-  export pixie.superImage
-  export pixie.draw
-  export pixie.fillGradient
-  export pixie.fillText
-  export pixie.strokeText
-  export pixie.fillPath
-  export pixie.strokePath
-  export pixie.newContext
+  export
+    pixie.writeFile,
+    pixie.wh,
+    pixie.copy,
+    pixie.getColor,
+    pixie.setColor,
+    pixie.fill,
+    pixie.flipHorizontal,
+    pixie.flipVertical,
+    pixie.subImage,
+    pixie.minifyBy2,
+    pixie.magnifyBy2,
+    pixie.applyOpacity,
+    pixie.invert,
+    pixie.blur,
+    pixie.newMask,
+    pixie.resize,
+    pixie.shadow,
+    pixie.superImage,
+    pixie.draw,
+    pixie.fillGradient,
+    pixie.fillText,
+    pixie.strokeText,
+    pixie.fillPath,
+    pixie.strokePath,
+    pixie.newContext
 
 exportRefObject Mask, ["width", "height"]:
-  export pixie.writeFile
-  export pixie.wh
-  export pixie.copy
-  export pixie.getValue
-  export pixie.setValue
-  export pixie.fill
-  export pixie.minifyBy2
-  export pixie.spread
-  export pixie.ceil
-  export pixie.newImage
-  export pixie.applyOpacity
-  export pixie.invert
-  export pixie.blur
-  export pixie.draw
-  export pixie.fillText
-  export pixie.strokeText
-  export pixie.fillPath
-  export pixie.strokePath
+  export
+    pixie.writeFile,
+    pixie.wh,
+    pixie.copy,
+    pixie.getValue,
+    pixie.setValue,
+    pixie.fill,
+    pixie.minifyBy2,
+    pixie.spread,
+    pixie.ceil,
+    pixie.newImage,
+    pixie.applyOpacity,
+    pixie.invert,
+    pixie.blur,
+    pixie.draw,
+    pixie.fillText,
+    pixie.strokeText,
+    pixie.fillPath,
+    pixie.strokePath
 
 exportRefObject Paint, ["*"]:
-  export pixie.newPaint
+  export
+    pixie.newPaint
 
 exportRefObject Path, ["*"]:
-  export pixie.transform
-  export pixie.addPath
-  export pixie.closePath
-  export pixie.computeBounds
-  export pixie.fillOverlaps
-  export pixie.strokeOverlaps
+  export
+    pixie.transform,
+    pixie.addPath,
+    pixie.closePath,
+    pixie.computeBounds,
+    pixie.fillOverlaps,
+    pixie.strokeOverlaps
 
   toggleBasicOnly()
 
-  export pixie.moveTo
-  export pixie.lineTo
-  export pixie.bezierCurveTo
-  export pixie.quadraticCurveTo
-  export pixie.ellipticalArcTo
-  export pixie.arc
-  export pixie.arcTo
-  export pixie.rect
-  export pixie.roundedRect
-  export pixie.ellipse
-  export pixie.circle
-  export pixie.polygon
+  export
+    pixie.moveTo,
+    pixie.lineTo,
+    pixie.bezierCurveTo,
+    pixie.quadraticCurveTo,
+    pixie.ellipticalArcTo,
+    pixie.arc,
+    pixie.arcTo,
+    pixie.rect,
+    pixie.roundedRect,
+    pixie.ellipse,
+    pixie.circle,
+    pixie.polygon
 
 exportRefObject Typeface, ["*"]:
-  export pixie.ascent
-  export pixie.descent
-  export pixie.lineGap
-  export pixie.lineHeight
-  export pixie.getGlyphPath
-  export pixie.getAdvance
-  export pixie.getKerningAdjustment
-  export pixie.newFont
+  export
+    pixie.ascent,
+    pixie.descent,
+    pixie.lineGap,
+    pixie.lineHeight,
+    pixie.getGlyphPath,
+    pixie.getAdvance,
+    pixie.getKerningAdjustment,
+    pixie.newFont
 
 exportRefObject Font, ["*"]:
-  export pixie.scale
-  export pixie.defaultLineHeight
-  export pixie.typeset
-  export pixie.computeBounds
+  export
+    pixie.scale,
+    pixie.defaultLineHeight,
+    pixie.typeset,
+    pixie.computeBounds
 
 exportRefObject Span, ["*"]:
   discard
 
 exportRefObject Arrangement, []:
-  export pixie.computeBounds
+  export
+    pixie.computeBounds
 
 exportRefObject Context, ["*"]:
-  export pixie.save
-  export pixie.saveLayer
-  export pixie.restore
-  export pixie.beginPath
-  export pixie.closePath
-  export pixie.fill
-  export pixie.clip
-  export pixie.stroke
-  export pixie.measureText
-  export pixie.getTransform
-  export pixie.setTransform
-  export pixie.transform
-  export pixie.resetTransform
-  export bindings.drawImage1
-  export bindings.drawImage2
-  export bindings.drawImage3
+  export
+    pixie.save,
+    pixie.saveLayer,
+    pixie.restore,
+    pixie.beginPath,
+    pixie.closePath,
+    pixie.fill,
+    pixie.clip,
+    pixie.stroke,
+    pixie.measureText,
+    pixie.getTransform,
+    pixie.setTransform,
+    pixie.transform,
+    pixie.resetTransform,
+    bindings.drawImage1,
+    bindings.drawImage2,
+    bindings.drawImage3
 
   toggleBasicOnly()
 
-  export pixie.moveTo
-  export pixie.lineTo
-  export pixie.bezierCurveTo
-  export pixie.quadraticCurveTo
-  export pixie.ellipticalArcTo
-  export pixie.arc
-  export pixie.arcTo
-  export pixie.rect
-  export pixie.roundedRect
-  export pixie.ellipse
-  export pixie.circle
-  export pixie.polygon
-  export pixie.clearRect
-  export pixie.fillRect
-  export pixie.strokeRect
-  export pixie.fillText
-  export pixie.strokeText
-  export pixie.translate
-  export pixie.scale
-  export pixie.rotate
-  export pixie.isPointInPath
-  export pixie.isPointInStroke
+  export
+    pixie.moveTo,
+    pixie.lineTo,
+    pixie.bezierCurveTo,
+    pixie.quadraticCurveTo,
+    pixie.ellipticalArcTo,
+    pixie.arc,
+    pixie.arcTo,
+    pixie.rect,
+    pixie.roundedRect,
+    pixie.ellipse,
+    pixie.circle,
+    pixie.polygon,
+    pixie.clearRect,
+    pixie.fillRect,
+    pixie.strokeRect,
+    pixie.fillText,
+    pixie.strokeText,
+    pixie.translate,
+    pixie.scale,
+    pixie.rotate,
+    pixie.isPointInPath,
+    pixie.isPointInStroke
 
 exportProcs:
-  export pixie.newImage
-  export pixie.newMask
-  export pixie.newPaint
-  export pixie.newPath
-  export pixie.newSpan
-  export pixie.newContext
-  export pixie.readImage
-  export pixie.readmask
-  export pixie.readTypeface
-  export pixie.readFont
-  export pixie.parsePath
-  export pixie.miterLimitToAngle
-  export pixie.angleToMiterLimit
+  export
+    pixie.newImage,
+    pixie.newMask,
+    pixie.newPaint,
+    pixie.newPath,
+    pixie.newSpan,
+    pixie.newContext,
+    pixie.readImage,
+    pixie.readmask,
+    pixie.readTypeface,
+    pixie.readFont,
+    pixie.parsePath,
+    pixie.miterLimitToAngle,
+    pixie.angleToMiterLimit
 
 writeFiles("bindings/generated", "pixie")
 
