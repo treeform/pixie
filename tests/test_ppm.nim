a7388a291d88d1a3f76f476a6e3af30521d99cc2
Rasmus Moorats <xx@nns.ee>
2022-01-11 12:51:54 +0200

    assert that generated images match P6 master

--- tests/test_ppm.nim
+++ tests/test_ppm.nim
@@ -11,3 +11,8 @@ block:
     "tests/fileformats/ppm/feep.p3.hidepth.master.ppm"
   ))
   writeFile("tests/fileformats/ppm/feep.p3.hidepth.ppm", encodePpm(image))
+
+  # produced output should be identical to P6 master
+  let p6Master = readFile("tests/fileformats/ppm/feep.p6.master.ppm")
+  for image in @["p3", "p6", "p3.hidepth"]:
+    doAssert readFile("tests/fileformats/ppm/feep." & $image & ".ppm") == p6Master
