b32d6d7c18d6ecbaf2fb00236afefd81ebed018b
treeform <starplant@gmail.com>
2022-03-18 23:28:03 -0700

    There is even less.

--- tests/test_paths.nim
+++ tests/test_paths.nim
@@ -1,18 +1,11 @@
-import pixie
 
 block:
   echo "??? stroke zero polygon ???"
-  # let image = newImage(200, 200)
-  # image.fill(rgba(255, 255, 255, 255))
-
-  # let ctx = newContext(image)
-  # ctx.setLineDash(@[2.0.float32])
-
   try:
     echo "inside the try"
-    raise newException(PixieError, "Just the exception please")
+    raise newException(ValueError, "Just the exception please")
     echo "no exception wut?"
-  except PixieError:
+  except ValueError:
     echo "getCurrentExceptionMsg"
     echo getCurrentExceptionMsg()
 
