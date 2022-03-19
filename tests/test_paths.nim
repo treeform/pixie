5283f0a2193c91bd008e9364a537342ad7f46310
treeform <starplant@gmail.com>
2022-03-18 23:21:42 -0700

    wut

--- tests/test_paths.nim
+++ tests/test_paths.nim
@@ -723,16 +723,18 @@ block:
 
 block:
   echo "??? stroke zero polygon ???"
-  let image = newImage(200, 200)
-  image.fill(rgba(255, 255, 255, 255))
+  # let image = newImage(200, 200)
+  # image.fill(rgba(255, 255, 255, 255))
 
-  let ctx = newContext(image)
-  ctx.setLineDash(@[2.0.float32])
+  # let ctx = newContext(image)
+  # ctx.setLineDash(@[2.0.float32])
 
   try:
+    echo "inside the try"
     raise newException(PixieError, "Just the exception please")
-    echo "no exception"
+    echo "no exception wut?"
   except PixieError:
+    echo "getCurrentExceptionMsg"
     echo getCurrentExceptionMsg()
 
 block:
