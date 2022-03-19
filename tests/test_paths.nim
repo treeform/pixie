5755a58f2fcb1f2bfefc217fd7e0692dd6e5941e
treeform <starplant@gmail.com>
2022-03-19 13:43:27 -0700

    rm 1

--- tests/test_paths.nim
+++ tests/test_paths.nim
@@ -1,27 +1,6 @@
 import chroma, pixie, pixie/fileformats/png, strformat
 
 block:
-  echo "arcTo"
-  var
-    surface = newImage(256, 256)
-    ctx = newContext(surface)
-  surface.fill(rgba(255, 255, 255, 255))
-
-  let
-    p0 = vec2(230, 20)
-    p1 = vec2(90, 130)
-    p2 = vec2(20, 20)
-
-  ctx.beginPath()
-  ctx.moveTo(p0.x, p0.y)
-  ctx.arcTo(p1.x, p1.y, p2.x, p2.y, 50)
-  ctx.lineTo(p2.x, p2.y)
-  ctx.stroke()
-
-  surface.writeFile("tests/paths/arcTo1.png")
-
-
-block:
   echo "??? stroke zero polygon ???"
 
   try:
