efbb66e6ef8456f81a549a5cec3151d7129c335d
Ryan Oldenburg <ryan@guzba.com>
2022-06-03 14:31:06 -0500

    f

--- tests/test_tiff.nim
+++ tests/test_tiff.nim
@@ -3,4 +3,4 @@ import pixie, pixie/fileformats/tiff
 let
   t = decodeTiff(readFile("tests/fileformats/tiff/pc260001.tif"))
   image = newImage(t)
-image.writeFile("tests/fileformats/tiff/pc260001.png")
+# image.writeFile("tests/fileformats/tiff/pc260001.png")
