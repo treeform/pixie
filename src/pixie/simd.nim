138d88a9bc8b6bbcc43d43f3e5b3b82a3a1fbbe4
Ryan Oldenburg <ryan@guzba.com>
2022-07-07 16:22:06 -0500

    rename

--- src/pixie/simd.nim
+++ src/pixie/simd.nim
@@ -4,8 +4,7 @@ when defined(release):
   {.push checks: off.}
 
 when defined(amd64):
-  import nimsimd/runtimecheck, nimsimd/sse2, runtimechecked/avx,
-      runtimechecked/avx2
+  import nimsimd/runtimecheck, nimsimd/sse2, simd/avx, simd/avx2
 
   let
     cpuHasAvx* = checkInstructionSets({AVX})
