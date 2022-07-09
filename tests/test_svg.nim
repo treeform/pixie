d2d2fc636fe6546e509a865060eefbb53fea1839
treeform <starplant@gmail.com>
2022-07-08 23:46:18 -0700

    f

--- tests/test_svg.nim
+++ tests/test_svg.nim
@@ -17,14 +17,6 @@ const files = [
   "dragon2"
 ]
 
-# proc doDiff(rendered: Image, name: string) =
-#   rendered.writeFile(&"tests/fileformats/svg/rendered/{name}.png")
-#   let
-#     master = readImage(&"tests/fileformats/svg/masters/{name}.png")
-#     (diffScore, diffImage) = diff(master, rendered)
-#   echo &"{name} score: {diffScore}"
-#   diffImage.writeFile(&"tests/fileformats/svg/diffs/{name}.png")
-
 for file in files:
   let image = readImage(&"tests/fileformats/svg/{file}.svg")
   image.diffVs(&"tests/fileformats/svg/masters/{file}.png")
