7f62ac5810dfda0c6ab189058bfe2cb1ae42f7c7
Ryan Oldenburg <ryan@guzba.com>
2022-07-10 18:40:51 -0500

    start on neon

--- src/pixie/simd.nim
+++ src/pixie/simd.nim
@@ -4,15 +4,22 @@ export internal
 
 const allowSimd* = not defined(pixieNoSimd) and not defined(tcc)
 
-when allowSimd and defined(amd64):
-  import simd/sse2, simd/avx, simd/avx2
-  export sse2, avx, avx2
+when allowSimd:
+  when defined(amd64):
+    import simd/sse2, simd/avx, simd/avx2
+    export sse2, avx, avx2
 
-  when not defined(pixieNoAvx):
-    import nimsimd/runtimecheck
-    let
-      cpuHasAvx* = checkInstructionSets({AVX})
-      cpuHasAvx2* = checkInstructionSets({AVX, AVX2})
+    when not defined(pixieNoAvx):
+      import nimsimd/runtimecheck
+      let
+        cpuHasAvx* = checkInstructionSets({AVX})
+        cpuHasAvx2* = checkInstructionSets({AVX, AVX2})
 
-  import nimsimd/sse2 as nimsimdsse2
-  export nimsimdsse2
+    import nimsimd/sse2 as nimsimdsse2
+    export nimsimdsse2
+
+  elif defined(arm64):
+    import simd/neon
+
+    import nimsimd/neon as nimsimdneon
+    export nimsimdneon
