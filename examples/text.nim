2ef52dc88012c4447fb4b1cd11166e7c5b357bf1
Simon Krauter <trustable@disroot.org>
2023-10-02 21:56:49 -0300

    Extend example text.nim
    Set the font color. (Did not find this in the documentation.)

--- examples/text.nim
+++ examples/text.nim
@@ -5,6 +5,7 @@ image.fill(rgba(255, 255, 255, 255))
 
 var font = readFont("examples/data/Roboto-Regular_1.ttf")
 font.size = 20
+font.paint.color = color(1, 0, 0)
 
 let text = "Typesetting is the arrangement and composition of text in graphic design and publishing in both digital and traditional medias."
 
